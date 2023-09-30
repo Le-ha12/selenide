0a49eaa89c2afef7b35d8a3504bf94f9d6a27111
Marakin IQA-65 <le-ha12@yandex.ru>
2023-09-30 11:12:50 +0300

    Initial commit

---
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -0,0 +1,81 @@
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.Configuration;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class CardDeliveryTest {
+
+    @BeforeEach
+    public void openPage() {
+        open("http://localhost:9999/");
+    }
+
+
+    @Test
+    public void shouldReturnSuccessIfFieldsAreFilledInCorrectly() {
+
+        LocalDate deliveryDateCard = LocalDate.now().plusDays(3);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String dateText = deliveryDateCard.format(formatter);
+
+        $("[data-test-id=city] [placeholder='Город']").setValue("Белгород");
+        $("[data-test-id=date] [class='input__box'] [placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] [class='input__box'] [placeholder='Дата встречи']").setValue(dateText);
+        $("[data-test-id=name] [type=text]").setValue("Иван Иванов");
+        $("[data-test-id=phone] [type=tel]").setValue("+79999999999");
+        $("[data-test-id=agreement]").click();
+        $("[role=button] .button__content").click();
+        $(withText("Успешно!")).shouldBe(Condition.visible, Duration.ofMillis(15000));
+        $("[data-test-id='notification'] .notification__content")
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + dateText));
+    }
+
+
+    @Test
+    public void shouldReturnSuccessfullyIfSurnameWithHyphen() {
+        LocalDate deliveryDateCard = LocalDate.now().plusDays(3);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String dateText = deliveryDateCard.format(formatter);
+
+        $("[data-test-id=city] [placeholder='Город']").setValue("Белгород");
+        $("[data-test-id=date] [class='input__box'] [placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] [class='input__box'] [placeholder='Дата встречи']").setValue(dateText);
+        $("[data-test-id=name] [type=text]").setValue("Иван Иванов-Смирнов");
+        $("[data-test-id=phone] [type=tel]").setValue("+79999999999");
+        $("[data-test-id=agreement]").click();
+        $("[role=button] .button__content").click();
+        $(withText("Успешно!")).shouldBe(Condition.visible, Duration.ofMillis(15000));
+        $("[data-test-id='notification'] .notification__content")
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + dateText));
+    }
+
+
+    @Test
+    public void shouldReturnSuccessfullyIfCityWithHyphen() {
+        LocalDate deliveryDateCard = LocalDate.now().plusDays(3);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String dateText = deliveryDateCard.format(formatter);
+
+        $("[data-test-id=city] [placeholder='Город']").setValue("Ростов-на-Дону");
+        $("[data-test-id=date] [class='input__box'] [placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] [class='input__box'] [placeholder='Дата встречи']").setValue(dateText);
+        $("[data-test-id=name] [type=text]").setValue("Иван Иванов-Смирнов");
+        $("[data-test-id=phone] [type=tel]").setValue("+79999999999");
+        $("[data-test-id=agreement]").click();
+        $("[role=button] .button__content").click();
+        $(withText("Успешно!")).shouldBe(Condition.visible, Duration.ofMillis(15000));
+        $("[data-test-id='notification'] .notification__content")
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + dateText));
+    }
+}
